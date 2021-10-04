23c0024b323f7b655237b1e43502b15c65c9005d
LexDX <fredom100000@yandex.ru>
2021-10-04 14:33:21 +0300

    fix dependencies, change imports

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { DxDataGridModule, DxPopupModule,  DxListModule } from "devextreme-angular";
+import { DxDataGridModule } from "devextreme-angular/ui/data-grid";
+import { DxPopupModule } from "devextreme-angular/ui/popup";
+import { DxListModule } from "devextreme-angular/ui/list";
 import { AppComponent } from './app.component';
 import { ColumnChooserComponent } from './column.chooser/column.chooser.component';
 
