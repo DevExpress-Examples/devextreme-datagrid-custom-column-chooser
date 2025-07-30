584ffc04a296cdd056fe9616e54e26486caeae85
Tom <artem.kurchenko@devexpress.com>
2025-07-30 12:02:02 +0400

    fix jQuery

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,123 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  $('#gridContainer').dxDataGrid({
+    dataSource: customers,
+    columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
+    showBorders: true,
+    onToolbarPreparing(e) {
+      const customColumnChooser = createColumnChooser(e.component);
+
+      e.toolbarOptions.items.push({
+        widget: 'dxButton',
+        options: {
+          icon: 'columnchooser',
+          onClick() {
+            customColumnChooser.show();
+          },
+        },
+        location: 'after',
+      });
     },
   });
 });
+
+function createColumnChooser(grid) {
+  const columns = grid.option('columns').map((column) => ({
+    dataField: column,
+    caption: captionize(column),
+  }));
+
+  // Create List
+  const list = $('<div>').dxList({
+    dataSource: columns,
+    selectedItems: columns,
+    displayExpr: 'caption',
+    searchExpr: 'caption',
+    searchEnabled: true,
+    showSelectionControls: true,
+    selectionMode: 'all',
+  }).dxList('instance');
+
+  // Create Popup
+  const popup = $('#customColumnChooser').dxPopup({
+    title: 'Column Chooser',
+    container: grid.element(),
+    contentTemplate() { return list.element(); },
+    width: 250,
+    height: 350,
+    resizeEnabled: true,
+    shading: false,
+    position: {
+      at: 'right bottom',
+      my: 'right bottom',
+    },
+    toolbarItems: [{
+      widget: 'dxButton',
+      location: 'after',
+      toolbar: 'bottom',
+      options: {
+        text: 'Apply',
+        onClick(e) {
+          const selectedItems = list.option('selectedItems');
+          grid.beginUpdate();
+          columns.forEach((column) => {
+            grid.columnOption(column.dataField, 'visible', selectedItems.includes(column));
+          });
+          grid.endUpdate();
+          popup.hide();
+        },
+      },
+    }, {
+      widget: 'dxButton',
+      location: 'after',
+      toolbar: 'bottom',
+      options: {
+        text: 'Cancel',
+        onClick(e) {
+          popup.hide();
+        },
+      },
+    }],
+  }).dxPopup('instance');
+
+  // Sync selected items
+  grid.on('optionChanged', (e) => {
+    if (e.name === 'columns' && e.fullName.includes('visible')) {
+      const columnIndex = parseInt(e.fullName.charAt(8), 10);
+      if (e.value) {
+        list.selectItem(columnIndex);
+      } else {
+        list.unselectItem(columnIndex);
+      }
+    }
+  });
+
+  return popup;
+}
+
+const DIGIT_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function captionize(name) {
+  const captionList = [];
+  let char;
+  let isPrevCharNewWord = false;
+  let isNewWord = false;
+
+  for (let i = 0; i < name.length; i += 1) {
+    char = name.charAt(i);
+    isNewWord = (char === char.toUpperCase() && char !== '-' && char !== ')' && char !== '/') || (char in DIGIT_CHARS);
+    if (char === '_' || char === '.') {
+      char = ' ';
+      isNewWord = true;
+    } else if (i === 0) {
+      char = char.toUpperCase();
+      isNewWord = true;
+    } else if (!isPrevCharNewWord && isNewWord) {
+      if (captionList.length > 0) {
+        captionList.push(' ');
+      }
+    }
+    captionList.push(char);
+    isPrevCharNewWord = isNewWord;
+  }
+  return captionList.join('');
+}
